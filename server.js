9f54eaf287d3d44a2f5eef6b672e45efe3192c22
Alex99y <Alexyammine94@gmail.com>
2018-09-08 09:23:37 -0400

    Added all request DoctorParticipant

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ var admin = require('./lib/participants/admin');
 var perfilpersonal = require('./lib/assets/perfilpersonal');
 var perfilpublico = require('./lib/assets/perfilpublico');
 var paciente = require('./lib/participants/patient');
+var doctor = require('./lib/participants/doctor');
+
 
 // Ping the network
 app.post('/ping',upload.single('card'), function Ping(req,res,next) {
@@ -16,6 +18,37 @@ app.post('/ping',upload.single('card'), function Ping(req,res,next) {
     res.send(response);
 });
 
+/* --------- PARTICIPANT DOCTOR --------- */
+// Create
+app.post('/participant/createDoctor', upload.single('card'), function CreateDoctor(req,res,next){
+    var response;
+    doctor.create(req,res,function(res){response = res;})
+    res.send(response);
+});
+// Get all
+app.post('/participant/getDoctors', upload.single('card'), function getDoctors(req,res,next){
+    var response;
+    doctor.getAll(req,res,function(res){response = res;})
+    res.send(response);
+});
+// Get by id
+app.post('/participant/getDoctorsById', upload.single('card'), function getDoctorsById(req,res,next){
+    var response;
+    doctor.getById(req,res,function(res){response = res;})
+    res.send(response);
+});
+// Update
+app.post('/participant/updateDoctor', upload.single('card'), function updateDoctor(req,res,next){
+    var response;
+    doctor.update(req,res,function(res){response = res;})
+    res.send(response);
+});
+// Delete
+app.post('/participant/deleteDoctor', upload.single('card'), function deleteDoctor(req,res,next){
+    var response;
+    doctor.delete(req,res,function(res){response = res;})
+    res.send(response);
+});
 
 /* --------- PARTICIPANT PATIENT -------- */
 // Create
@@ -42,7 +75,7 @@ app.post('/participant/updatePatient', upload.single('card'), function updatePat
     paciente.update(req,res,function(res){response = res;})
     res.send(response);
 });
-// DElete
+// Delete
 app.post('/participant/deletePatient', upload.single('card'), function deletePatient(req,res,next){
     var response;
     paciente.delete(req,res,function(res){response = res;})
