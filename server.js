410779fc3e14350bddabd582cfc7e8753c82ef06
Alex99y <Alexyammine94@gmail.com>
2018-09-09 16:55:04 -0400

    giveAccess Medical History

--- server.js
+++ server.js
@@ -10,45 +10,51 @@ var perfilpublico = require('./lib/assets/perfilpublico');
 var paciente = require('./lib/participants/patient');
 var doctor = require('./lib/participants/doctor');
 var historial = require('./lib/assets/historialmedico');
-
+var giveaccess = require('./lib/transactions/giveAccess');
 process.title = "api-medical-chain";
 
 // Ping the network
 app.post('/ping',upload.single('card'), function Ping(req,res,next) {
     var response;
-    composer.ping(req,function(res){response = res;})
+    composer.ping(req,res,function(res){response = res;});
     res.send(response);
 });
 
+/* ------------ TRANSACTIONS ------------ */
+app.post('/transaction/giveAccess', upload.single('card'), function giveAccess(req,res,next){
+    var response;
+    giveaccess.giveAccess(req,res,function(res){response = res;});
+    res.send(response);
+});
 /* --------- PARTICIPANT DOCTOR --------- */
 // Create
 app.post('/participant/createDoctor', upload.single('card'), function CreateDoctor(req,res,next){
     var response;
-    doctor.create(req,res,function(res){response = res;})
+    doctor.create(req,res,function(res){response = res;});
     res.send(response);
 });
 // Get all
 app.post('/participant/getDoctors', upload.single('card'), function getDoctors(req,res,next){
     var response;
-    doctor.getAll(req,res,function(res){response = res;})
+    doctor.getAll(req,res,function(res){response = res;});
     res.send(response);
 });
 // Get by id
 app.post('/participant/getDoctorsById', upload.single('card'), function getDoctorsById(req,res,next){
     var response;
-    doctor.getById(req,res,function(res){response = res;})
+    doctor.getById(req,res,function(res){response = res;});
     res.send(response);
 });
 // Update
 app.post('/participant/updateDoctor', upload.single('card'), function updateDoctor(req,res,next){
     var response;
-    doctor.update(req,res,function(res){response = res;})
+    doctor.update(req,res,function(res){response = res;});
     res.send(response);
 });
 // Delete
 app.post('/participant/deleteDoctor', upload.single('card'), function deleteDoctor(req,res,next){
     var response;
-    doctor.delete(req,res,function(res){response = res;})
+    doctor.delete(req,res,function(res){response = res;});
     res.send(response);
 });
 
@@ -56,31 +62,31 @@ app.post('/participant/deleteDoctor', upload.single('card'), function deleteDoct
 // Create
 app.post('/participant/createPatient', upload.single('card'), function CreatePatient(req,res,next){
     var response;
-    paciente.create(req,res,function(res){response = res;})
+    paciente.create(req,res,function(res){response = res;});
     res.send(response);
 });
 // Get all
 app.post('/participant/getPatients', upload.single('card'), function getPatients(req,res,next){
     var response;
-    paciente.getAll(req,res,function(res){response = res;})
+    paciente.getAll(req,res,function(res){response = res;});
     res.send(response);
 });
 // Get by id
 app.post('/participant/getPatientsById', upload.single('card'), function getPatientsById(req,res,next){
     var response;
-    paciente.getById(req,res,function(res){response = res;})
+    paciente.getById(req,res,function(res){response = res;});
     res.send(response);
 });
 // Update
 app.post('/participant/updatePatient', upload.single('card'), function updatePatient(req,res,next){
     var response;
-    paciente.update(req,res,function(res){response = res;})
+    paciente.update(req,res,function(res){response = res;});
     res.send(response);
 });
 // Delete
 app.post('/participant/deletePatient', upload.single('card'), function deletePatient(req,res,next){
     var response;
-    paciente.delete(req,res,function(res){response = res;})
+    paciente.delete(req,res,function(res){response = res;});
     res.send(response);
 });
 
@@ -88,31 +94,31 @@ app.post('/participant/deletePatient', upload.single('card'), function deletePat
 // Create
 app.post('/participant/createAdmin', upload.single('card'), function CreateAdmin(req,res,next){
     var response;
-    admin.create(req,res,function(res){response = res;})
+    admin.create(req,res,function(res){response = res;});
     res.send(response);
 });
 // Get all
 app.post('/participant/getAdmins', upload.single('card'), function getAdmins(req,res,next){
     var response;
-    admin.getAll(req,res,function(res){response = res;})
+    admin.getAll(req,res,function(res){response = res;});
     res.send(response);
 });
 // Get by id
 app.post('/participant/getAdminsById', upload.single('card'), function getAdminsById(req,res,next){
     var response;
-    admin.getById(req,res,function(res){response = res;})
+    admin.getById(req,res,function(res){response = res;});
     res.send(response);
 });
 // Update
 app.post('/participant/updateAdmin', upload.single('card'), function updateAdmin(req,res,next){
     var response;
-    admin.update(req,res,function(res){response = res;})
+    admin.update(req,res,function(res){response = res;});
     res.send(response);
 });
 // Delete
 app.post('/participant/deleteAdmin', upload.single('card'), function deleteAdmin(req,res,next){
     var response;
-    admin.delete(req,res,function(res){response = res;})
+    admin.delete(req,res,function(res){response = res;});
     res.send(response);
 });
 
